33ef52c43d9f7430c181999cb99361a9f658f25e
Anton Markelov <doublic@gmail.com>
2017-11-14 14:05:37 +1000

    limit message length before sending

--- main.go
+++ main.go
@@ -109,6 +109,14 @@ func loadTemplate(tmplPath string) *template.Template {
 	return tmpH
 }
 
+func CutString(s string, i int) string {
+	runes := []rune(s)
+	if len(runes) > i {
+		return string(runes[:i])
+	}
+	return s
+}
+
 func main() {
 	flag.Parse()
 
@@ -303,7 +311,7 @@ func POST_Handling(c *gin.Context) {
 	log.Println(msgtext)
 	log.Println("+-----------------------------------------------------------+")
 
-	msg := tgbotapi.NewMessage(chatid, msgtext)
+	msg := tgbotapi.NewMessage(chatid, CutString(msgtext, 4096))
 	msg.ParseMode = tgbotapi.ModeHTML
 
 	msg.DisableWebPagePreview = true
